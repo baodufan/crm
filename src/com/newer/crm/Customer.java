76e582588bf5801b1dbcd7702b10f08b7acf7d4b
baodu <360175613@qq.com>
2017-05-24 14:37:26 +0800

    针对user类进行了修改

--- src/com/newer/crm/Customer.java
+++ src/com/newer/crm/Customer.java
@@ -2,11 +2,31 @@ package com.newer.crm;
 
 public class Customer {
 
-	int id;
-	String name;
-	String phone;
-	
-	public static void main(String[] args) {
-		System.out.println("id"+"name");
+	private int id;
+	private String name;
+	private String phone;
+	public int getId() {
+		return id;
 	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", phone=" + phone + "]";
+	}
+	
+	
 }
